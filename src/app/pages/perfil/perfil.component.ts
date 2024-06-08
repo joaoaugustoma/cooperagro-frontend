1b41e75d92bb816eea0b45872935a787e8d439a2
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2024-06-08 11:30:09 -0300

    Tela Perfil e Categorias

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../../components/navbar/navbar.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-    imports: [
-        NavbarComponent
-    ],
+  imports: [
+    NavbarComponent,
+    RouterLink
+  ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
 export class PerfilComponent {
+  nomeUsuario: string = 'João Augusto Moreira Ananias';
 
+  logout() {
+
+  }
 }
