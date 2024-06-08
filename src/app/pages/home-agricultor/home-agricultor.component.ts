b3d3dfd3e921e999d8a86494f7b29fbe33c46471
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2024-06-08 14:15:04 -0300

    Home Agricultor

--- src/app/pages/home-agricultor/home-agricultor.component.ts
+++ src/app/pages/home-agricultor/home-agricultor.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {NavbarAgricultorComponent} from "../../components/navbar-agricultor/navbar-agricultor.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home-agricultor',
   standalone: true,
   imports: [
-    NavbarAgricultorComponent
+    NavbarAgricultorComponent,
+    RouterLink
   ],
   templateUrl: './home-agricultor.component.html',
   styleUrl: './home-agricultor.component.scss'
 })
 export class HomeAgricultorComponent {
+  nomeLoja: string = 'Loja A';
+  qtdProdutosVendidos: string = '14.850';
+  ganhos: string = 'R$ 1.485,00';
 
 }
