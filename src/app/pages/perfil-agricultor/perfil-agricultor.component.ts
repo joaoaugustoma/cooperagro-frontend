9c2e64c918366538812016a99f0b4421a90d90ec
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2024-06-09 19:41:04 -0300

    Perfil Agricultor

--- src/app/pages/perfil-agricultor/perfil-agricultor.component.ts
+++ src/app/pages/perfil-agricultor/perfil-agricultor.component.ts
@@ -1,15 +1,43 @@
 import { Component } from '@angular/core';
 import {NavbarAgricultorComponent} from "../../components/navbar-agricultor/navbar-agricultor.component";
+import {NavbarComponent} from "../../components/navbar/navbar.component";
+import {Router, RouterLink} from "@angular/router";
+import {LogoutComponent} from "../../components/logout/logout.component";
+import {MatDialog} from "@angular/material/dialog";
+import {LoginService} from "../../services/login.service";
 
 @Component({
   selector: 'app-perfil-agricultor',
   standalone: true,
     imports: [
-        NavbarAgricultorComponent
+        NavbarAgricultorComponent,
+        NavbarComponent,
+        RouterLink
     ],
   templateUrl: './perfil-agricultor.component.html',
   styleUrl: './perfil-agricultor.component.scss'
 })
 export class PerfilAgricultorComponent {
+  nomeLoja: string = 'Loja A';
 
+  constructor(public dialog: MatDialog,
+              private router: Router,
+              private loginService: LoginService) {}
+
+  logout() {
+    const dialogRef = this.dialog.open(LogoutComponent, {
+      width: '400px',
+      height: '400px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.loginService.logout();
+        this.router.navigate(['/']);
+      } else {
+        this.router.navigate(['/perfil']);
+      }
+    });
+  }
 }
