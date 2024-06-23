3199610715693b608783092ad282c46719a1898d
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2024-06-23 19:17:25 -0300

    Listagem de pedido

--- src/app/pages/cliente/pedidos/pedidos.component.ts
+++ src/app/pages/cliente/pedidos/pedidos.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {PedidoVendaService} from "../../../services/pedido-venda.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-pedidos',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './pedidos.component.html',
   styleUrl: './pedidos.component.scss'
 })
-export class PedidosComponent {
+export class PedidosComponent implements OnInit {
+  pedidos: any[] = [];
+  produtosPedido: any[] = [];
 
-  constructor( private router: Router) {
+  constructor(private router: Router,
+              private pedidoVendaService: PedidoVendaService) {
+  }
+
+  ngOnInit() {
+    this.pedidoVendaService.getPedidosByEmailUsuario().subscribe(
+      (response) => {
+        console.log(response)
+        this.pedidos = response;
+        this.pedidos.forEach(pedido => {
+          this.produtosPedido = pedido.carrinhoCompra.produtos;
+        })
+      })
   }
 
   navigateToPerfil() {
