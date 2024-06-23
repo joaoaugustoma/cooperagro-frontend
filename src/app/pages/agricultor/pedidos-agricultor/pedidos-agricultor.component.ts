201761e18d49457dd02bd2be6a6509c737c323ee
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2024-06-23 17:51:27 -0300

    Pedidos Agricultor

--- src/app/pages/agricultor/pedidos-agricultor/pedidos-agricultor.component.ts
+++ src/app/pages/agricultor/pedidos-agricultor/pedidos-agricultor.component.ts
@@ -1,16 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {CurrencyPipe, DatePipe, NgForOf} from "@angular/common";
+import {LoginService} from "../../../services/login.service";
+import {PedidoVendaService} from "../../../services/pedido-venda.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-pedidos',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    DatePipe,
+    NgForOf
+  ],
   templateUrl: './pedidos-agricultor.component.html',
   styleUrl: './pedidos-agricultor.component.scss'
 })
-export class PedidosAgricultorComponent {
+export class PedidosAgricultorComponent implements OnInit{
+  pedidos: any[] = [];
+  produtosPedido: any[] = [];
 
-  constructor( private router: Router) {
+  constructor(private router: Router,
+              private pedidoVendaService: PedidoVendaService,
+              private toastr: ToastrService) {}
+
+  ngOnInit() {
+    this.carregarPedidos(); // Chamando o método para carregar pedidos
+  }
+
+  carregarPedidos() {
+    this.pedidoVendaService.getPedidoVenda().subscribe(
+      (data: any[]) => {
+        if(!data)
+          this.toastr.error('Sem pedidos!');
+        console.log(data)
+        this.pedidos = data;
+        this.pedidos.forEach(pedido => {
+          this.produtosPedido = pedido.carrinhoCompra.produtos;
+
+        })
+      },
+      (error) => {
+        console.error('Erro ao carregar pedidos', error);
+      }
+    );
   }
 
   navigateToPerfil() {
